cae59f2a4eb142131dcc0be47ff5b1fb1dae8881
mh <m.e.harms@t-online.de>
2016-05-26 18:13:18 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ def read( fname ):
     return open( os.path.join( os.path.dirname(__file__), fname ) ).read()
 
 setup(
-    name='easywebapp',
-    version = "0.1.0",
+    name='easy-web-app',
+    version = "0.1.1",
     packages = ["easywebapp"],
     #scripts = ['webapp.py'],
 
